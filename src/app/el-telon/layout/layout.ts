9906b8915caf4593d9d55d2cb92dd010d8ade922
issa23i <isabelpastorlopezcv@gmail.com>
2025-07-19 13:43:05 +0200

    feat: layout

--- src/app/el-telon/layout/layout.ts
+++ src/app/el-telon/layout/layout.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [
+    RouterOutlet,
+    Footer,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './layout.html',
   styleUrls: ['./layout.scss'],
 })
