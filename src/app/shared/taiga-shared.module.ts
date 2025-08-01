16489cad9f6b95a18472132e41fdcd10d7b0f726
issa23i <isabelpastorlopezcv@gmail.com>
2025-08-01 21:20:34 +0200

    feat: shared modules

---
+++ src/app/shared/taiga-shared.module.ts
@@ -0,0 +1,68 @@
+// taiga-shared.module.ts
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Taiga UI standalone components
+import {
+  TuiButton,
+  TuiTextfield,
+  TuiSelect,
+  TuiHint,
+  TuiError,
+  TuiLabel,
+} from '@taiga-ui/core';
+import {
+  TuiCheckbox,
+  TuiFieldErrorPipe,
+  TuiInputChipComponent,
+  TuiTextarea,
+} from '@taiga-ui/kit';
+import { TuiForm } from '@taiga-ui/layout';
+import { TuiInputModule, TuiInputPhoneModule } from '@taiga-ui/legacy';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Taiga UI
+    TuiForm,
+    TuiButton,
+    TuiSelect,
+    TuiCheckbox,
+    TuiTextarea,
+    TuiInputChipComponent,
+    TuiError,
+    TuiLabel,
+    TuiFieldErrorPipe,
+    TuiInputModule,
+
+    TuiInputPhoneModule,
+    ...TuiTextfield,
+    ...TuiHint,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Taiga UI
+    ...TuiTextfield,
+    ...TuiHint,
+
+    TuiFieldErrorPipe,
+    TuiLabel,
+    TuiForm,
+    TuiError,
+    TuiButton,
+    TuiSelect,
+    TuiCheckbox,
+    TuiTextarea,
+    TuiInputChipComponent,
+    TuiInputPhoneModule,
+    TuiInputModule,
+  ],
+})
+export class TaigaSharedModule {}
