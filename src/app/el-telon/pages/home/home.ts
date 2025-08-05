321bc570f54d92e36034ade21b299940f132d3b6
issa23i <isabelpastorlopezcv@gmail.com>
2025-08-05 19:00:45 +0200

    feat: inicio

--- src/app/el-telon/pages/home/home.ts
+++ src/app/el-telon/pages/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButton, TuiTitle } from '@taiga-ui/core';
+import { TuiCarousel, TuiPagination } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor, TuiCarousel, TuiPagination, TuiTitle, TuiButton],
   templateUrl: './home.html',
-  styleUrls: ['./home.scss']
+  styleUrls: ['./home.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
-
+  protected index = 2;
+  items = [
+    '“El teatro es poesía que se sale del libro para hacerse humana.”\n — Federico García Lorca',
+    '“La cultura es el alma de los pueblos.”',
+    '“Ama el arte en ti mismo, no a ti mismo en el arte.”\n —  Constantin Stanislavski ',
+    '“El escenario es el lugar donde la verdad se disfraza para ser más sincera.”',
+    '“La cultura no es un lujo, es una necesidad.”\n — Gao Xingjian',
+  ];
 }
