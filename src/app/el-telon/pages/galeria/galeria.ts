43386eb8ec2c431bea31e23368256a67d25d8a78
issa23i <isabelpastorlopezcv@gmail.com>
2025-08-18 18:04:03 +0200

    feat: galería

--- src/app/el-telon/pages/galeria/galeria.ts
+++ src/app/el-telon/pages/galeria/galeria.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { TuiDialogService } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './galeria.html',
-  styleUrls: ['./galeria.scss']
+  styleUrls: ['./galeria.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Galeria {
+  @ViewChild('dialogTemplate', { static: true })
+  dialogTemplate!: TemplateRef<any>;
 
+  dialogService = inject(TuiDialogService);
+
+  images = [
+    {
+      src: 'assets/img/ENSAYO1.jpeg',
+      alt: 'Ensayo 1',
+      caption: 'Primera lectura',
+    },
+    {
+      src: 'assets/img/ENSAYO2.jpg',
+      alt: 'Ensayo 2',
+      caption: 'Concentración antes del ensayo',
+    },
+    {
+      src: 'assets/img/ENSAYO3.jpeg',
+      alt: 'Ensayo 3',
+      caption: 'Conociendo el personaje',
+    },
+  ];
+
+  verImagen(img: { src: string; alt: string; caption: string }): void {
+    this.dialogService
+      .open(this.dialogTemplate, {
+        label: img.alt,
+        data: img,
+        size: 'l',
+      })
+      .subscribe();
+  }
 }
