8d8ae82c421a5fe96f4d7c514492501ffeda0ca9
issa23i <isabelpastorlopezcv@gmail.com>
2025-08-23 21:39:54 +0200

    feat: añadir formulario contacto

--- src/app/el-telon/pages/casting/casting.ts
+++ src/app/el-telon/pages/casting/casting.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaigaSharedModule } from '../../../shared/taiga-shared.module';
+import { Contacto } from '../contacto/contacto';
 
 @Component({
   selector: 'app-casting',
   standalone: true,
-  imports: [TaigaSharedModule],
+  imports: [TaigaSharedModule, Contacto],
   templateUrl: './casting.html',
   styleUrls: ['./casting.scss'],
 })
