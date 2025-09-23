ff87e7a3efc6f73dd9d521ad05f920bf0b9269f6
issa23i <isabelpastorlopezcv@gmail.com>
2025-09-23 23:18:45 +0200

    feat: publicacion castings

--- src/app/el-telon/pages/casting/casting.ts
+++ src/app/el-telon/pages/casting/casting.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Contacto } from '../contacto/contacto';
 import { TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-casting',
   standalone: true,
-  imports: [TuiTitle, TuiHeader, Contacto],
+  imports: [CommonModule, TuiTitle, TuiHeader, Contacto],
   templateUrl: './casting.html',
   styleUrls: ['./casting.scss'],
 })
