0ae15d495388e87a28daf664342a865c9a7330c4
issa23i <isabelpastorlopezcv@gmail.com>
2026-06-06 19:12:42 +0200

    feat: firma federación y refactor componente news

--- src/app/shared/news/news.ts
+++ src/app/shared/news/news.ts
@@ -1,4 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  booleanAttribute,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Input,
+} from '@angular/core';
 import { TuiButton, TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
 
@@ -10,4 +15,18 @@ import { TuiHeader } from '@taiga-ui/layout';
   styleUrl: './news.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class News {}
+export class News {
+  @Input() firstTitle = '¿Te quedaste fuera en el último casting?';
+
+  @Input() paragraphText =
+    'No te preocupes. EL TELÓN – Asociación cultural de artes escénicas abrirá proximamente dos nuevas fechas para los castings dirigidas a personas interesadas en actuar, colaborar en producción o participar en talleres escénicos. La convocatoria estará abierta a todos los públicos, sin necesidad de experiencia previa.';
+
+  @Input() secondTitle =
+    'Estate a atento/a a nuestra web y redes. Si tienes alguna puedes escribirnos a nuestro correo.';
+
+  @Input({ transform: booleanAttribute }) hasButton = true;
+
+  @Input() imageSrc = '';
+
+  @Input() imageAlt = '';
+}
